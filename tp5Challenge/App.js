90ff6508427d0cb8ed84bdcd3252396e86772582
Matiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <102532863+MatiMCode@users.noreply.github.com>
2022-08-04 10:24:32 -0300

    comitealo

--- tp5Challenge/App.js
+++ tp5Challenge/App.js
@@ -1,17 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+
 import axios from 'axios'; 
 
 const client = axios.create({ baseURL: 'https://dog.ceo/api/breeds/image/random'});
 
-const get = async () =>{  //export
-  return client.get('').then(response => response.data)
-  .catch(error => {
-    console.log(error)
-    throw error;
-  });
-}
 
 const post = async (data) =>{ //export
   return client.post('', {...data}).then(response => response.data)
@@ -26,9 +20,37 @@ const post = async (data) =>{ //export
 
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [perro, setPerro] = useState('')
+  const getPerro = async () =>{
+    console.log('loading')
+    setLoading(true);
+    axios.get('https://dog.ceo/api/breeds/image/random', {})
+      .then(function (response) {
+       // console.log(response.data.message +'a');
+       console.log('loaded')
+
+       setLoading(false)
+        setPerro(response.data.message)
+        console.log(perro)
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+      .then(function () {
+          // always executed
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>AUGHHHHHHHHHHHHH</Text>
+      <Button onPress={getPerro} title='press for doggo'/>
+      <Text>{loading?'loading':'loaded'}</Text>
+      <Image
+        style={styles.stretch}
+        source={perro}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -40,5 +62,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },stretch: {
+    width: 200,
+    height: 200,
+    resizeMode: 'stretch',
   },
 });
