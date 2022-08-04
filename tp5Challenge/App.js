e6a51ca10aafd460ef73863e57e7ca3bc657078b
Matiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <102532863+MatiMCode@users.noreply.github.com>
2022-08-04 08:57:23 -0300

    AUGH

--- tp5Challenge/App.js
+++ tp5Challenge/App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'; 
+
+const client = axios.create({ baseURL: 'https://dog.ceo/api/breeds/image/random'});
+
+export const get = async () =>{
+  return client.get('').then(response => response.data)
+  .catch(error => {
+    console.log(error)
+    throw error;
+  });
+}
+
+export const post = async (data) =>{
+  return client.post('', {...data}).then(response => response.data)
+  .catch(error => {
+    console.log(error)
+    throw error;
+  });
+}
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>AUGHHHHHHHHHHHHH</Text>
-      <Button>AAAAAAA</Button>
       <StatusBar style="auto" />
     </View>
   );
